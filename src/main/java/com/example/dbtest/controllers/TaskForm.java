79a2bdb23ed9331d12644dee5fa663a2f6f756a7
Famfam <famfam.lancers@gmail.com>
2018-11-03 21:52:10 +0900

    NoArgsConstructorを追加 lombokのバージョンの問題か？

--- src/main/java/com/example/dbtest/controllers/TaskForm.java
+++ src/main/java/com/example/dbtest/controllers/TaskForm.java
@@ -2,6 +2,7 @@ package com.example.dbtest.controllers;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.springframework.format.annotation.DateTimeFormat;
 
 import javax.validation.constraints.NotEmpty;
@@ -9,7 +10,7 @@ import java.io.Serializable;
 import java.time.LocalDateTime;
 
 @Data
-//@NoArgsConstructor
+@NoArgsConstructor
 @AllArgsConstructor
 public class TaskForm implements Serializable {
 
